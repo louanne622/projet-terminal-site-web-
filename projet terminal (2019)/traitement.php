73e8911a6e5e5fd1364b81baf7d95cf4032c72d4
Lou-Anne Lecocq <115995772+louanne622@users.noreply.github.com>
2023-01-12 16:45:07 +0100

    implémentation fichier github

---
+++ projet terminal (2019)/traitement.php
@@ -0,0 +1,23 @@
+
+<?php
+    $n=$_POST['field2'];
+    $p=$_POST['field1'];
+    echo "<p>Bonjour ".$p." ".$n.", votre inscription pour le marathon de Ablain-Saint-Nazaire a bien été enregistrée. Nous vous enverrons plus d'informations sur votre boîte mail.</p>";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
